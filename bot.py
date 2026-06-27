673dcd29ae19621150120a2f0d78963b7c226318
bilyknikolas-lgtm <bilyknikolas@gmail.com>
2026-06-27 20:12:10 +0200

    Update bot.py
    Telegram bot

--- bot.py
+++ bot.py
@@ -1,31 +1,46 @@
 import telebot
+import smtplib
+from email.mime.text import MIMEText
 
-bot = telebot.TeleBot("7631628545:AAFS978oXDfE6R8e8gj7aOGf4tRzOx94gqg")
+TOKEN = "7631628545:AAFS978oXDfE6R8e8gj7aOGf4tRzOx94gqg"
 
-user_step = {}
+EMAIL = "bilyknikolas@gmail.com"
+APP_PASSWORD = "ggme nqte bjee xvnm"
+
+bot = telebot.TeleBot(TOKEN)
+
 user_name = {}
+
+def send_email(name, file_id):
+    msg = MIMEText(f"Имя: {name}\nВидео file_id: {file_id}")
+    msg["Subject"] = "Новое видео из Telegram бота"
+    msg["From"] = EMAIL
+    msg["To"] = EMAIL
+
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(EMAIL, APP_PASSWORD)
+    server.send_message(msg)
+    server.quit()
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, "Здравствуйте")
-    user_step[message.chat.id] = 1
+    bot.send_message(message.chat.id, "Здравствуйте. Введите имя")
 
 @bot.message_handler(content_types=['text'])
-def text_handler(message):
-    chat_id = message.chat.id
-
-    if user_step.get(chat_id) == 1:
-        user_name[chat_id] = message.text
-        bot.send_message(chat_id, "Отправьте видео")
-        user_step[chat_id] = 2
+def get_name(message):
+    user_name[message.chat.id] = message.text
+    bot.send_message(message.chat.id, "Теперь отправьте видео")
 
 @bot.message_handler(content_types=['video'])
-def video_handler(message):
+def get_video(message):
     chat_id = message.chat.id
+    name = user_name.get(chat_id, "неизвестно")
 
-    if user_step.get(chat_id) == 2:
-        name = user_name.get(chat_id, "неизвестно")
-        bot.send_message(chat_id, f"Видео получено от: {name}")
-        user_step[chat_id] = 1
+    file_id = message.video.file_id
+
+    send_email(name, file_id)
+
+    bot.send_message(chat_id, "Видео отправлено на почту")
 
 bot.infinity_polling()
