23880385a393be5561ba08e7ef4e800c83bf3bd2
bilyknikolas-lgtm <bilyknikolas@gmail.com>
2026-06-27 22:06:16 +0200

    bot.py
    bot.py

--- bot.py
+++ bot.py
@@ -22,8 +22,7 @@ def get_video(message):
 
     name = user_name.get(chat_id, "неизвестно")
 
-    bot.send_message(chat_id, f" Видео отправлено\nИмя: {name}")
+    bot.send_message(chat_id, "Видео отправлено\nИмя: " + name)
 
-# 🔥 ОБЯЗАТЕЛЬНО В КОНЦЕ
 if __name__ == "__main__":
     bot.infinity_polling()
